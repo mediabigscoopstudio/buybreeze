0fc338131d80cbc377d54dc1e48ee91bc09df547
mediabigscoopstudio <media.bigscoopstudio@gmail.com>
2026-05-11 13:15:07 +0530

    Static Files Dir Change

--- corecrm/settings.py
+++ corecrm/settings.py
@@ -100,11 +100,12 @@ USE_TZ = True
 # ============================================================
 # STATIC & MEDIA
 # ============================================================
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+import os 
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static",]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+MEDIA_URL = '/media/' 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
