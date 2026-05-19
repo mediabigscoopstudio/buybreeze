c8377f42963c8ff87cf1301f03f5c84a147cb5d2
Media BigScoop Studio <media.bigscoopstudio@gmail.com>
2026-05-20 02:11:00 +0530

    Manager Page

--- manager/urls.py
+++ manager/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from manager import views
 from django.conf import settings
-from django.conf.urls.static import static
-from dash.views import apr_report
-from dash.views import apr_report, individual_apr_report
+from django.conf.urls.static import static 
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -13,6 +11,4 @@ urlpatterns = [
     path('assign-to-tl/', views.assign_to_tl, name='assign_to_tl'),
     path('unassign_lead/<int:lead_id>/', views.unassign_lead, name='unassign_lead'),
     path('tl_performance/<int:id>/', views.tl_performance, name='tl_performance'),
-    path('apr-report/', apr_report, name='manager_apr_report'),
-    path('apr-report/<str:username>/', individual_apr_report, name='individual_apr_report'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
