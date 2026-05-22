7e870b8f97393f0813924199db9dece9a0a9c056
Media BigScoop Studio <media.bigscoopstudio@gmail.com>
2026-05-22 11:26:47 +0530

    Update API Issues

--- employee/urls.py
+++ employee/urls.py
@@ -13,10 +13,10 @@ urlpatterns = [
     path("verify-otp/", views.verify_otp, name="verify_otp"),
     path("logout/", views.logout_view, name="logout_view"),
     #android APP
-    path("api/auth/send-otp/",android.send_otp, "send_otp"),
-    path("api/auth/verify-otp/",android.verify_otp, "verify_otp"),
-    path("api/attendance/punch-out/",android.punch_out, "punch_out"),
-    path("api/attendance/status/",android.attendance_status, "attendance_status"),
+    path("api/auth/send-otp/", android.send_otp, name="send_otp"),
+    path("api/auth/verify-otp/", android.verify_otp, name="verify_otp"),
+    path("api/attendance/punch-out/", android.punch_out, name="punch_out"),
+    path("api/attendance/status/", android.attendance_status, name="attendance_status"),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
