b39f988a2a555227694e7475b1769a861037d846
Media BigScoop Studio <media.bigscoopstudio@gmail.com>
2026-05-22 11:39:57 +0530

    Update API Issues

--- employee/urls.py
+++ employee/urls.py
@@ -1,22 +1,18 @@
 from django.urls import path
-from employee import views
-from django.conf import settings
-from django.conf.urls.static import static
-from django.urls import path
 from . import views
 from . import android
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    #web
+    # web
     path("", views.index, name="index"),
     path("login/", views.login_view, name="login_view"),
-    path("verify-otp/", views.verify_otp, name="verify_otp"),
+    path("verify-otp/", views.verify_otp, name="web_verify_otp"),
     path("logout/", views.logout_view, name="logout_view"),
-    #android APP
-    path("api/auth/send-otp/", android.send_otp, name="send_otp"),
-    path("api/auth/verify-otp/", android.verify_otp, name="verify_otp"),
-    path("api/attendance/punch-out/", android.punch_out, name="punch_out"),
-    path("api/attendance/status/", android.attendance_status, name="attendance_status"),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
+    # android API
+    path("api/auth/send-otp/", android.send_otp, name="api_send_otp"),
+    path("api/auth/verify-otp/", android.verify_otp, name="api_verify_otp"),
+    path("api/attendance/punch-out/", android.punch_out, name="api_punch_out"),
+    path("api/attendance/status/", android.attendance_status, name="api_attendance_status"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
